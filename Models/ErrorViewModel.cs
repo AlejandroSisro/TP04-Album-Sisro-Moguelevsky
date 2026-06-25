b295673acdc72575f8b25e5e8671229180dcde8d
ALEJANDRO MOISES SISRO MELES <50434059@ort>
2026-06-25 09:06:24 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Album_Sisro_Moguelevsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
