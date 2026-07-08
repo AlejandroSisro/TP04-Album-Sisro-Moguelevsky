46f99e1cd581508240b1ead4e570675511dc37aa
50233806-create <50233806@est.ort.edu.ar>
2026-07-08 08:46:12 -0300

    Enhance Figuritas class with new properties
    Added properties for IdFigurita, NumeroCamiseta, Jugadores, and Seleccion to the Figuritas class.

--- Models/Figuritas.cs
+++ Models/Figuritas.cs
@@ -2,4 +2,11 @@ namespace TP04_Album_Sisro_Moguelevsky.Models;
 
 public class Figuritas
 {
+    private int IdFigurita;
+    private int NumeroCamiseta; 
+    private string Jugadores; 
+    private string Seleccion;
+}
+public class Figuritas
+{
 }
