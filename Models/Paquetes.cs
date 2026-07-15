bd0820d574a3bb812a2c41ac76de7468e3a4e209
50233806-create <50233806@est.ort.edu.ar>
2026-07-15 08:28:34 -0300

    Refactor Paquete class with properties and improved methods

--- Models/Paquetes.cs
+++ Models/Paquetes.cs
@@ -2,17 +2,18 @@ namespace TP04_Album_Sisro_Moguelevsky.Models;
 
 public class Paquete
 {
-    private int idPaquete;
-    private DateTime fechaApertura;
-    private int usuarioId;
-    private List<Jugador> figuritas= new List<Jugador>();
-    private void AgregarFigurita(Jugador jugador)
+    public int IdPaquete { get; set; }
+    public DateTime FechaApertura { get; set; }
+    public int UsuarioId { get; set; }
+    public List<Jugador> Figuritas { get; set; } = new List<Jugador>();
+    public void AgregarFigurita(Jugador jugador)
     {
         Figuritas.Add(jugador);
     }
+
     public int CantidadFiguritas()
     {
-        int cantidad = Figurita.Count;
-        return cantidad;
+        int CantidadFiguritas=Figuritas.Count;
+        return CantidadFiguritas;
     }
 }
