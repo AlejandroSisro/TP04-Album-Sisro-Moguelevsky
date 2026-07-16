17225e63dd730c5d90b30279298ead69c4ac1ac5
50233806-create <50233806@est.ort.edu.ar>
2026-07-16 08:36:43 -0300

    Refactor Jugadores class to simplify NombreCompleto
    Removed the last name property and adjusted the NombreCompleto method to return only the first name.

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -4,16 +4,15 @@ public class Jugadores
 {
     public int idJugador { get; set; }
     public string nombre { get; set; }
-    public string Apellido { get; set; }
     public string foto { get; set; }
     public int idSeleccion { get; set; }
-    public jugador()
+
+    public Jugadores()
     {
     }
 
     public string NombreCompleto()
     {
-        string nombreCompleto = Nombre + " " + Apellido;
-        return nombreCompleto;
+        return nombre;
     }
 }
